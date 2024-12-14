1c5c77e126c6e89bcf820f1d890fc19d08a9734b
hubgood98 <heej1998@gmail.com>
2024-12-14 22:01:31 +0900

    포트변경 6709

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,9 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    port:6709,
+    strictPort: true, // 사용 중이면 에러 발생 (자동으로 다른 포트로 변경하지 않음)
   }
 })
